7e680bf9d1b843b6deea7a557ba2a30af68d75ec
AhmedIzaz <ahmedizazasif@gmail.com>
2026-04-04 22:19:05 +0600

    compose file updated

--- src/config/swagger.config.ts
+++ src/config/swagger.config.ts
@@ -2,11 +2,13 @@ import { INestApplication } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 const setupSwagger = (intance: INestApplication) => {
-  const config = new DocumentBuilder()
-    .setTitle('Personal Medical Assistant API')
-    .addBearerAuth()
-    .build();
-  const documentFactory = () => SwaggerModule.createDocument(intance, config);
-  SwaggerModule.setup('api/docsAcheEiRoute', intance, documentFactory);
+    const config = new DocumentBuilder()
+        .setTitle('Personal Medical Assistant API')
+        .addBearerAuth()
+        .build();
+    const documentFactory = () => SwaggerModule.createDocument(intance, config);
+    const route = process.env.API_BASE + '/api/docsAcheEiRoute';
+    console.log("route", route)
+    SwaggerModule.setup(route, intance, documentFactory);
 };
 export default setupSwagger;
