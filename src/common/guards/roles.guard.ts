48998d05e597a7e21eed1af0507d61bcdb16ee78
AhmedIzaz <ahmedizazasif@gmail.com>
2026-05-02 18:16:55 +0600

    console removed

--- src/common/guards/roles.guard.ts
+++ src/common/guards/roles.guard.ts
@@ -23,7 +23,7 @@ export class RolesGuard implements CanActivate {
 
     const request = context.switchToHttp().getRequest();
     const user = request.user;
-    console.log(user)
+
 
     if (!user?.actorType || !requiredRoles.includes(user.actorType)) {
       throw new ForbiddenException(
